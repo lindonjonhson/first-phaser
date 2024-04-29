6e600d2fb555fce3a10a4e412681567745d2a8f7
Lindon Johnson Farias De Morais Junior <linjofdmj@meta.com>
2024-04-29 14:58:03 +0100

    Finished Start and End scenes

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 import './style.css'
 import Phaser from 'phaser'
 
+const gameStartDiv = document.querySelector('#gameStartDiv')
+const gameStartBtn = document.querySelector('#gameStartBtn')
+const gameEndDiv = document.querySelector('#gameEndDiv')
+const gameWinLoseSpan = document.querySelector('#gameWinLoseSpan')
+const gameEndScoreSpan = document.querySelector('#gameEndScoreSpan')
+
+
 //  Creating obj Scene
 class GameScene extends Phaser.Scene{
     constructor(){
@@ -9,13 +16,31 @@ class GameScene extends Phaser.Scene{
         this.cursor
         this.playerSpeed = speedDown * 5
         this.target
+        this.textScore
+        this.points=0
+        this.textTime
+        this.timedEvent
+        this.remainingTime
+        this.coinMusic
+        this.bgMusic
+        this.emitter
     }
     preload(){
         this.load.image('bg', '/assets/bg.png')
         this.load.image('bsk', '/assets/basket.png')
         this.load.image('apl', '/assets/apple.png')
+        this.load.image('money', '/assets/money.png')
+        this.load.audio('bgMusic', '/assets/bgMusic.mp3')
+        this.load.audio('coin', '/assets/coin.mp3')
     }
     create(){
+
+        this.scene.pause('scene-game')
+
+        this.coinMusic = this.sound.add('coin')
+        this.bgMusic = this.sound.add('bgMusic')
+
+        //this.bgMusic.play()
         //  Adding background
         this.add.image(0,0,'bg').setOrigin(0,0)
 
@@ -46,8 +71,35 @@ class GameScene extends Phaser.Scene{
         //  Creating a function which will detect when the apple hits the basket
         this.physics.add.overlap(this.target, this.player, targetHit, null, this)
 
+        //  Adding Text Score
+        this.textScore = this.add.text(sizes.width -120, 10, 'Score:0',{
+            font:'25px Arial',
+            fill:'#000000'
+        })
+
+        //  Adding Text Time
+        this.textTime = this.add.text(10, 10, 'Remainig Time: 00',{
+            font:'25px Arial',
+            fill:'#000000'
+        })
+
+        this.timedEvent = this.time.delayedCall(3000, gameOver, [], this)
+
+        this.emitter = this.add.particles(0, 0,'money',{
+            speed:100,
+            gravityY:speedDown-200,
+            scale:0.04,
+            duration:100,
+            emitting:false
+        })
+        this.emitter.startFollow(this.player, this.player.width/2, this.player.height/2, true)
+
     }
     update(){
+
+        this.remainingTime = this.timedEvent.getRemainingSeconds()
+        this.textTime.setText(`Remaining Time: ${Math.round(this.remainingTime).toString()}`)
+
         //  Adding the cursor
         const{left, right} = this.cursor
 
@@ -64,6 +116,9 @@ class GameScene extends Phaser.Scene{
             this.target.setY(0);
             this.target.setX(getRandomX());
         }
+
+
+
     }
 }
 
@@ -74,7 +129,8 @@ const sizes={
 }
 
 //  Setting up gravity force
-const speedDown = 300
+const speedDown = 150
+
 
 const config ={
     type:Phaser.WEBGL,
@@ -94,14 +150,36 @@ const config ={
     scene:[GameScene]
 }
 
-const game = new Phaser.Game(config)
 
 function getRandomX() {
     return Math.floor(Math.random() * 480);
 }
 
 function targetHit(){
+    this.coinMusic.play()
     this.target.setY(0);
     this.target.setX(getRandomX());
     this.points++;
+    this.textScore.setText(`Score: ${this.points}`)
+    this.emitter.start()
 }
+
+function gameOver(){
+    console.log('Game Over')
+    this.sys.game.destroy(true)
+    gameEndDiv.style.display='flex'
+    if(this.points >=10){
+        gameEndScoreSpan.textContent = this.points
+        gameWinLoseSpan.textContent = 'Win!'
+    }else{
+        gameEndScoreSpan.textContent = this.points
+        gameWinLoseSpan.textContent = 'Lose!'
+    }
+}
+
+const game = new Phaser.Game(config)
+
+gameStartBtn.addEventListener('click', ()=>(
+    gameStartDiv.style.display='none',
+    game.scene.resume('scene-game')
+))
